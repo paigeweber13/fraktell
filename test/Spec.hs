baf463ebd719545c48d111a33753880aa8a6dc2a
Riley Weber <rileyw13@protonmail.com>
2019-10-25 16:42:28 -0700

    fixed test

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,6 +1,7 @@
+import Control.Exception (evaluate)
+import Data.Complex
 import Test.Hspec
 import Test.QuickCheck
-import Control.Exception (evaluate)
 
 import CommonFunctions
 
@@ -8,8 +9,8 @@ main :: IO ()
 main = hspec $ do
   -- test CommonFunctions
   describe "CommonFunctions.f1" $ do
-    it "f(c, z) where c = 1 + 1i and z = 2 is 5 + 1i"
-      f1 (1.0 :+ 1.0) 2.0 `shouldBe` (5.0 +: 1.0)
+    it "f(c, z) where c = 1 + 1i and z = 2 is 5 + 1i" $
+      f1 (1.0 :+ 1.0) 2.0 `shouldBe` (5.0 :+ 1.0)
 
     it "returns the first element of a list" $ do
       head [23 ..] `shouldBe` (23 :: Int)
