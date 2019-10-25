81ced2f08d74d3a342c3e569e4206394cbbd963c
Riley Weber <rileyw13@protonmail.com>
2019-10-25 16:52:16 -0700

    wrote tests for basic function f1

--- test/Spec.hs
+++ test/Spec.hs
@@ -12,11 +12,11 @@ main = hspec $ do
     it "f(c, z) where c = 1 + 1i and z = 2 is 5 + 1i" $
       f1 (1.0 :+ 1.0) 2.0 `shouldBe` (5.0 :+ 1.0)
 
-    it "returns the first element of a list" $ do
-      head [23 ..] `shouldBe` (23 :: Int)
+    it "f(c, z) where c = 7.2 + 9.2i and z = 3.2 is 17.44 + 9.2i" $
+      f1 (7.2 :+ 9.2) 3.2 `shouldBe` (17.44 :+ 9.2)
 
-    it "returns the first element of an *arbitrary* list" $
-      property $ \x xs -> head (x:xs) == (x :: Int)
-
-    it "throws an exception if used with an empty list" $ do
-      evaluate (head []) `shouldThrow` anyException
+    it "should use partial application to create function with constant c" $ do
+      let f2 = f1 (3.1 :+ 4.8)
+      f2 5 `shouldBe` (28.1 :+ 4.8)
+      f2 5.5 `shouldBe` (33.35 :+ 4.8)
+      f2 6 `shouldBe` (39.1 :+ 4.8)
