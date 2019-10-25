cddecf5ecabaa6d80a904838bfca293d0c5b2bd4
Riley Weber <rileyw13@protonmail.com>
2019-10-25 10:25:52 -0700

    trying to write my own tests. Doesn't work yet

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,21 @@
+import Test.Hspec
+import Test.QuickCheck
+import Control.Exception (evaluate)
+
+import CommonFunctions
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  -- test CommonFunctions
+  describe "CommonFunctions.f1" $ do
+    it "f(c, z) where c = 1 + 1i and z = 2 is 5 + 1i"
+      f1 (1.0 :+ 1.0) 2.0 `shouldBe` (5.0 +: 1.0)
+
+    it "returns the first element of a list" $ do
+      head [23 ..] `shouldBe` (23 :: Int)
+
+    it "returns the first element of an *arbitrary* list" $
+      property $ \x xs -> head (x:xs) == (x :: Int)
+
+    it "throws an exception if used with an empty list" $ do
+      evaluate (head []) `shouldThrow` anyException
