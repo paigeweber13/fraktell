a0d94c2d1662f22f1ea9fdb39de203a7f362ef68
Riley Weber <rileyw13@protonmail.com>
2019-10-26 10:47:55 -0700

    z may now be a complex number

--- src/CommonFunctions.hs
+++ src/CommonFunctions.hs
@@ -3,8 +3,8 @@ module CommonFunctions where
 import Data.Complex
 
 -- f(z) = z^2 + c
-f1 :: Complex Double -> Double -> Complex Double
-f1 (a :+ b) z = (z**2 :+ 0) + (a :+ b)
+f1 :: Complex Double -> Complex Double -> Complex Double
+f1 c z = z**2 + c
 
 -- below is valid haskell
 -- f1 :: Double -> Double -> Double
