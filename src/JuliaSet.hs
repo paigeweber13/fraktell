f916572c9fb202a2eb57fe525170566f30498952
Riley Weber <rileyw13@protonmail.com>
2019-10-26 10:57:02 -0700

    started writing funtion to compute julia set

--- src/JuliaSet.hs
+++ src/JuliaSet.hs
@@ -1,1 +1,19 @@
 module JuliaSet where
+
+import Data.Complex
+
+-- type EscapeRadius = Double
+-- type Width = Double
+-- type Height = Double
+
+-- make this work for arbitrary funciton (use arbitrary number of params?)
+-- computeJuliaSet :: (Double -> Complex Double) -> Double -> Double -> Double
+computeJuliaSet :: (Complex Double -> Complex Double)
+                -> Double 
+                -> Double 
+                -> Double 
+                -> Int
+                -> [[Double]]
+computeJuliaSet f escapeRadius width height maxIter = 
+  output
+  where output = [[]]
