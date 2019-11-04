5f722b8e74263a79e4f496e20bcd0c2eb3b42505
Riley <rileyw13@protonmail.com>
2019-11-04 17:46:55 -0500

    uses command line arguments

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,30 @@
 module Main where
 
 import Data.Complex
+import System.Environment
 
 import CommonFunctions
 import JuliaSet
 
 main :: IO ()
 main = do
-  -- visualizeJuliaSet f 2 1000 1000 1000
-  visualizeJuliaSet f 2 1000 1000 100
+  args <- getArgs
+  if length args < 4
+  then usage
+  else do
+    putStrLn "visualizing julia set..."
+    visualizeJuliaSet f 
+      (read (args !! 0) :: Double)
+      (read (args !! 1) :: Int)
+      (read (args !! 2) :: Int)
+      (read (args !! 3) :: Int)
+    putStrLn "Done! Output to"
   where
     f = f1 ((-0.4) :+ 0.65)
+
+usage = do
+  prog <- getProgName
+  putStrLn ("Usage: " ++ prog ++ " escape_radius width height "
+    ++ "max_iter func_num [constant1 [constant2] ... ]")
+  putStrLn ("number of constants must correspond to the number of constants "
+    ++ "function func_num takes")
