ae4c45b02a927fc080b98623d6c9baad8b18bd14
Riley <rileyw13@protonmail.com>
2019-11-04 17:58:37 -0500

    UNFINISHED: working on CLI parsing
    currently moving most of main to puarseArgs and parseFunctionParams

--- app/Main.hs
+++ app/Main.hs
@@ -9,22 +9,40 @@ import JuliaSet
 main :: IO ()
 main = do
   args <- getArgs
-  if length args < 4
+  outputFilename <- (args !! 4)
+  if length args < 5
   then usage
-  else do
-    putStrLn "visualizing julia set..."
-    visualizeJuliaSet f 
-      (read (args !! 0) :: Double)
-      (read (args !! 1) :: Int)
-      (read (args !! 2) :: Int)
-      (read (args !! 3) :: Int)
-    putStrLn "Done! Output to"
+  else if length args < 6
+    then f = f1 ((-0.4) :+ 0.65)
+    else f = parseFunctionParams (read (args !! 6) :: Int)
+  putStrLn "visualizing julia set..."
+  visualizeJuliaSet f 
+    (read (args !! 0) :: Double)
+    (read (args !! 1) :: Int)
+    (read (args !! 2) :: Int)
+    (read (args !! 3) :: Int)
+    outputFilename
+  putStrLn ("Done! Output to " ++ outputFilename)
   where
     f = f1 ((-0.4) :+ 0.65)
+
+-- takes string cli args and returns processed args for visualizeJuliaSet
+parseArgs :: [String] -> (
+  (Complex Double -> Complex Double), Double, Int, Int, Int, String)
+parseArgs args
+  | (length args) < 5 = usage
+
+parseFunctionParams :: Integer
+                    -> [Complex]
+                    -> (Complex Double -> Complex Double)
+parseFunctionParams func_num params
+    | 
 
 usage = do
   prog <- getProgName
   putStrLn ("Usage: " ++ prog ++ " escape_radius width height "
-    ++ "max_iter func_num [constant1 [constant2] ... ]")
+    ++ "max_iter output_filename [func_num")
+  putStrLn ("                    [constant1 [constant2] ... ]]")
+  putStrLn ""
   putStrLn ("number of constants must correspond to the number of constants "
     ++ "function func_num takes")
