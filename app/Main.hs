923adc28180b571ec302511690dd272163297eb8
Riley <rileyw13@protonmail.com>
2019-11-04 18:18:23 -0500

    UNFINISHED: wrote out incorrect code that hopefully
    gets my intent across, will return to fix later

--- app/Main.hs
+++ app/Main.hs
@@ -9,34 +9,54 @@ import JuliaSet
 main :: IO ()
 main = do
   args <- getArgs
-  outputFilename <- (args !! 4)
-  if length args < 5
-  then usage
-  else if length args < 6
-    then f = f1 ((-0.4) :+ 0.65)
-    else f = parseFunctionParams (read (args !! 6) :: Int)
-  putStrLn "visualizing julia set..."
-  visualizeJuliaSet f 
-    (read (args !! 0) :: Double)
-    (read (args !! 1) :: Int)
-    (read (args !! 2) :: Int)
-    (read (args !! 3) :: Int)
-    outputFilename
-  putStrLn ("Done! Output to " ++ outputFilename)
-  where
-    f = f1 ((-0.4) :+ 0.65)
+  if length args < 5 then do
+    usage
+  else do
+    processedArgs <- parseArgs args
+    outputFilename <- (args !! 4)
+    putStrLn "visualizing julia set..."
+    visualizeJuliaSet 
+      (processedArgs !! 0)
+      (processedArgs !! 1)
+      (processedArgs !! 2)
+      (processedArgs !! 3)
+      (processedArgs !! 4)
+    putStrLn ("Done! Output to " ++ outputFilename)
 
 -- takes string cli args and returns processed args for visualizeJuliaSet
 parseArgs :: [String] -> (
   (Complex Double -> Complex Double), Double, Int, Int, Int, String)
 parseArgs args
-  | (length args) < 5 = usage
+  -- | (length args) < 5 = do
+  --     usage
+  --     putStrLn "using default values " ++ (show (
+  --       default_func, 1.5, 1000, 1000, 100, "images/output.png"))
+  --     (default_func, 1.5, 1000, 1000, 100, "images/output.png")
+  | (length args) < 6 = (default_func, r, width, height, maxIter, 
+                         outputFilename)
+  | (length args) < 7 = ((parseFunctionParams func_num []), r, width, height,
+                         maxIter, outputFilename)
+  | (length args) < 8 = ((parseFunctionParams func_num params), r, width,
+                         height, maxIter, outputFilename)
+  where
+    default_func = f1 ((-0.4) :+ 0.65)
+    r = read (args !! 0) :: Double
+    width = read (args !! 1) :: Int
+    height = read (args !! 2) :: Int
+    maxIter = read (args !! 3) :: Int
+    outputFilename = args !! 4
+    func_num = read (args!!5) :: Int
+    params = map (read::String -> Complex Double) (slice 6 (length args) args)
 
-parseFunctionParams :: Integer
-                    -> [Complex]
+slice :: Int -> Int -> [a] -> [a]
+slice from to xs = take (to - from + 1) (drop from xs)
+
+parseFunctionParams :: Int
+                    -> [Complex Double]
                     -> (Complex Double -> Complex Double)
 parseFunctionParams func_num params
-    | 
+    | func_num == 1 && params == [] = f1 ((-0.4) :+ 0.65)
+    | func_num == 1 = f1 (params!!0)
 
 usage = do
   prog <- getProgName
