339ec2bc6e5000574e6797a16b3d5886b1982714
Riley <rileyw13@protonmail.com>
2019-11-04 14:59:21 -0500

    wrote julia computation with recursion

--- src/JuliaSet.hs
+++ src/JuliaSet.hs
@@ -2,18 +2,42 @@ module JuliaSet where
 
 import Data.Complex
 
--- type EscapeRadius = Double
--- type Width = Double
--- type Height = Double
-
 -- make this work for arbitrary funciton (use arbitrary number of params?)
--- computeJuliaSet :: (Double -> Complex Double) -> Double -> Double -> Double
+--    alternatively, assume that whatever function will only take one value, z,
+--    as input, and other parameters can be applied through partial application
+
+-- in the output, x is the real portion of the result and y is the imaginary
+-- portion
 computeJuliaSet :: (Complex Double -> Complex Double)
                 -> Double 
-                -> Double 
-                -> Double 
+                -> Int
+                -> Int
                 -> Int
                 -> [[Double]]
-computeJuliaSet f escapeRadius width height maxIter = 
-  output
-  where output = [[]]
+computeJuliaSet f escapeRadius width height maxIter
+  = output
+  where 
+    output = [[]]
+    xstep = 2*escapeRadius/(fromIntegral width)
+    ystep = 2*escapeRadius/(fromIntegral height)
+
+julia:: (Complex Double -> Complex Double)
+      -> Complex Double
+      -> Double -- escapeRadius
+      -> Int -- maxIter
+      -> Int -- result
+julia f z escapeRadius maxIter = juliaRecursive f z escapeRadius 1 maxIter
+
+-- complex and real parts must be between -escapeRadius and escapeRadius
+juliaRecursive :: (Complex Double -> Complex Double)
+      -> Complex Double
+      -> Double -- escapeRadius
+      -> Int -- i
+      -> Int -- maxIter
+      -> Int -- result
+juliaRecursive f z escapeRadius i maxIter
+  | i > maxIter = 0
+  | magnitude(z') > escapeRadius = i
+  | otherwise = juliaRecursive f z' escapeRadius (i+1) maxIter
+    where
+      z' = f z
