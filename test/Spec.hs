30eba143bbee13a1d60e68d6e8aeb5f0dd40da32
Riley <rileyw13@protonmail.com>
2019-11-04 13:24:34 -0500

    tests now pass, improved descriptions
    floating point numbers are... messy...

--- test/Spec.hs
+++ test/Spec.hs
@@ -12,17 +12,17 @@ main = hspec $ do
     it "f1 should work with only real numbers" $
       f1 1 2 `shouldBe` 5
 
-    it "f1 should work with imaginary constant and real variable" $
+    it "f1 should work with complex constant and real variable" $
       f1 (1 :+ 1) 2 `shouldBe` (5 :+ 1)
 
-    it "f1 should work with real constant and imaginary variable" $
+    it "f1 should work with real constant and complex variable" $
       f1 2 (2 :+ 3) `shouldBe` ((-3) :+ 12)
 
     it "f1 should work with only complex numbers" $
-      f1 (7.2 :+ 9.2) (3.2 :+ 1.9) `shouldBe` (13.83 :+ 21.36)
+      f1 (7 :+ 9.25) (3 :+ 2) `shouldBe` (12 :+ 21.25)
 
     it "should use partial application to create function with constant c" $ do
-      let f2 = f1 (3.1 :+ 4.8)
-      f2 5 `shouldBe` (28.1 :+ 4.8)
-      f2 5.5 `shouldBe` (33.35 :+ 4.8)
-      f2 6 `shouldBe` (39.1 :+ 4.8)
+      let f2 = f1 (16 :+ 4)
+      f2 5 `shouldBe` (41 :+ 4)
+      f2 5.5 `shouldBe` (46.25 :+ 4)
+      f2 6 `shouldBe` (52 :+ 4)
