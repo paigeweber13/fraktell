e721fa79d8611fa3be4ccc160d263d79b2d2210a
Riley <rileyw13@protonmail.com>
2019-11-17 13:22:29 -0500

    added criterion dependency, added cli arg for benchmark

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 -- third party imports
+import Criterion.Main
 import Data.Complex
 import Data.Tuple
 import System.Environment
@@ -12,13 +13,25 @@ import JuliaSet
 main :: IO ()
 main = do
   args <- getArgs
-  if length args < 5 then do
-    usage
+  if length args > 0 && length args < 5 && (args !! 0) == "b" then do
+    visualizeJuliaBench
   else do
-    let outputFilename = args !! 4
-    putStrLn "visualizing julia set..."
-    runWithCliArgs args
-    putStrLn ("Done! Image output to " ++ outputFilename)
+    if length args < 5 then do
+      usage
+    else do
+      let outputFilename = args !! 4
+      putStrLn "visualizing julia set..."
+      runWithCliArgs args
+      putStrLn ("Done! Image output to " ++ outputFilename)
+
+visualizeJuliaBench = defaultMain [
+    bgroup "visualize julia set" [
+      bench "f1 ((-0.4) :+ 0.65) RSU" $ nfIO (visualizeJuliaSet default_func  
+        1.5 10000 10000 100 "images/output.png" "RSU")
+    ]
+  ]
+  where
+    default_func = f1 ((-0.4) :+ 0.65)
 
 -- takes string cli args and returns processed args for visualizeJuliaSet
 runWithCliArgs :: [String] -> IO()
@@ -61,9 +74,13 @@ parseFunctionParams func_num params
 usage = do
   let prog = "stack run"
   -- prog <- getProgName
-  putStrLn ("Usage: " ++ prog ++ " escape_radius width height "
+  putStrLn ("Usages: ") 
+  putStrLn ("    " ++ prog ++ " escape_radius width height "
     ++ "max_iter output_filename [func_num")
-  putStrLn ("                    [constant1 [constant2] ... ]]")
+  putStrLn ("              [constant1 [constant2] ... ]]")
+  putStrLn ("    " ++ prog ++ " b")
+  putStrLn ""
+  putStrLn "usage 2 (supplying only 'b' as a parameter) will run benchmark"
   putStrLn ""
   putStrLn ("number of constants must correspond to the number of constants "
     ++ "function func_num takes")
